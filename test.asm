5adc7f9ee97dd88b6e85458ce82f0b8203e7e38c
Stuart Haidon <serayen.sh@gmail.com>
2022-04-23 08:25:13 +0100

    Clean up test program.

--- test.asm
+++ test.asm
@@ -1,29 +1,23 @@
-; MMIO:
-;  - 0x80: 1
-;  - 0x81: 0
-;  - 0x82: RR
-;  - 0x83: !RR
-
-#DEF MMIO_ZERO = 0x80
-#DEF MMIO_ONE = 0x81
-#DEF MMIO_RR = 0x82
-#DEF MMIO_RRQ = 0x83
+#DEF MMIO_ZERO  = 0x80
+#DEF MMIO_ONE   = 0x81
+#DEF MMIO_RR    = 0x82
+#DEF MMIO_RRQ   = 0x83
 
 ; Initialize the CPU
-IEN MMIO_ONE    ; Enable input
-OEN MMIO_ONE    ; Enable output
-ONE             ; Result ALU
-LD MMIO_ZERO    ; Load 0 into MMIO_RR
+IEN     MMIO_ONE    ; Enable input
+OEN     MMIO_ONE    ; Enable output
+ONE                 ; Reset ALU
+LD      MMIO_ZERO   ; Load 0 into MMIO_RR
 
-LD MMIO_ONE     ; Load Op1 Bit1 into MMIO_RR
-ADD MMIO_ONE    ; Add Op2 Bit1 to MMIO_RR
-STOC 0x00       ; Store !MMIO_RR to RAM
+LD      MMIO_ONE    ; Load Op1 Bit1 into MMIO_RR
+Add     MMIO_ONE    ; Add Op2 Bit1 to MMIO_RR
+STOC    0x00        ; Store !MMIO_RR to RAM
 
-LD MMIO_ZERO    ; Load 0 into MMIO_RR
-ADD MMIO_ZERO   ; Add 0 to MMIO_RR (move Carry to RR)
-STO 0x1         ; Store MMIO_RR to RAM
+LD      MMIO_ZERO   ; Load 0 into MMIO_RR
+ADD     MMIO_ZERO   ; Add 0 to MMIO_RR (move Carry to RR)
+STO     0x1         ; Store MMIO_RR to RAM
 
-NAND MMIO_ONE   ; NAND MMIO_RR with 1
-OEN MMIO_RR     ; Disable OEN if RR is 0
-LD MMIO_ONE
-STO 0x3         ; Save to RAM. Write should be suppressed
+NAND    MMIO_ONE    ; NAND MMIO_RR with 1
+OEN     MMIO_RR     ; Disable OEN if RR is 0
+LD      MMIO_ONE
+STO     0x3         ; Save to RAM. Write should be suppressed
